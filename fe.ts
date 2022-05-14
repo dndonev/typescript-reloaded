625ceb2c892cd4b5900346b8643e8b6869dd4e87
Dobrin Donev <dobrindonev@gmail.com>
2022-05-14 10:16:58 +0300

    wul2

--- fe.ts
+++
@@ -1,9 +0,0 @@
-const changeFirstListItem: (value: string) => void = (value: string) => {
-    const ul: HTMLCollectionOf<HTMLUListElement> = document.getElementsByTagName('ul');
-
-    const firstItem: HTMLUListElement | null = ul.item(0);
-
-    if (firstItem) {
-        firstItem.textContent = value;
-    }
-};
